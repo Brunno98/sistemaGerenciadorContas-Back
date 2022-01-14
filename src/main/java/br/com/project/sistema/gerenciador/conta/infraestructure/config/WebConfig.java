f586a8354c600117b4ddc15ba953fd44ad61e479
teste <teste@example.com>
2022-01-13 22:54:03 -0300

    fix Cors

--- src/main/java/br/com/project/sistema/gerenciador/conta/infraestructure/config/WebConfig.java
+++ src/main/java/br/com/project/sistema/gerenciador/conta/infraestructure/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig implements WebMvcConfigurer{
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
-    
+
 }
